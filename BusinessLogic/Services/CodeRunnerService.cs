7c34abae2fb38970ac8c07d721ab822a42ae4e02
BGergo03 <banoczyg15@gmail.com>
2025-05-18 20:34:05 +0200

    WIP

---
+++ BusinessLogic/Services/CodeRunnerService.cs
@@ -0,0 +1,11 @@
+﻿using Persistence.Entities;
+
+namespace BusinessLogic.Services;
+
+public class CodeRunnerService : ICodeRunnerService
+{
+    public Task<string> RunCodeAsync(string code, Language language, IEnumerable<TestCase> testCases)
+    {
+        throw new NotImplementedException();
+    }
+}
